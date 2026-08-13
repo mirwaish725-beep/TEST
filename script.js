b965ef5a2d3e04bb76a0227f5175f27b937dc999
mirwaish725-beep <mirwaish725@gmail.com>
2026-08-13 06:21:41 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+const SUPABASE_URL = "اینجا Project URL را قرار بده";
+
+const SUPABASE_KEY = "اینجا Publishable key را قرار بده";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+
+async function testConnection() {
+
+    const status = document.getElementById(
+        "connection-status"
+    );
+
+    try {
+
+        const { data, error } = await supabaseClient
+            .from("customers")
+            .select("id")
+            .limit(1);
+
+        if (error) {
+            throw error;
+        }
+
+        status.textContent =
+            "✅ اتصال به دیتابیس با موفقیت انجام شد";
+
+        console.log("Supabase connected:", data);
+
+    } catch (error) {
+
+        status.textContent =
+            "❌ اتصال به دیتابیس انجام نشد";
+
+        console.error("Supabase Error:", error);
+    }
+}
+
+
+testConnection();
+
 function hello() {
     alert("سیستم سفارشات چاپخانه آماده است!");
 }
